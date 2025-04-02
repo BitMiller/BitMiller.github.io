dde23027141269d49432d98095a5acf4a1bd81ce
Peter Gall <gp.bitmiller@gmail.com>
2025-04-02 06:17:55 +0200

    at250402-3-061754

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>BitMiller's HomePage</title>
+</head>
+<body>
+<h3>Coming sooooon...</h3>
+<p>@250402-3-0541</p>
+
+<?php
+echo "<p>PHP test.</p>";
+?>
+
+</body>
+</html>
